5daf9aed80984efb77884d496a9dd138dabb4850
Roma4286 <lionkrou8@gmail.com>
2025-03-09 22:37:24 +0300

    init db: Initial db structure

---
+++ server/src/database.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from src.config import settings
+
+engine = create_engine(
+    url=settings.db_url,
+    echo=True,
+)
+
+session_factory = sessionmaker(engine)
+
+class Base(DeclarativeBase):
+    def save(self, session, with_commit: bool = True):
+        session.add(self)
+        if with_commit:
+            session.commit()
+
+    @classmethod
+    def get_object(cls, session, **filters):
+        query = session.query(cls)
+        for key, value in filters.items():
+            if value:
+                query = query.filter(getattr(cls, key) == value)
+        return query
+
+    def delete(self, session):
+        session.delete(self)
+        session.commit()
+
+def get_db():
+    db = session_factory()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ server/src/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base, engine
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(30))
+    password: Mapped[str] = mapped_column(String(255))
+
+    @classmethod
+    def get_object(cls, session, id=None, username=None, password=None, role=None):
+        query = super().get_object(session=session, id=id, username=username, password=password, role=role)
+        return query
+
+
+# Base.metadata.create_all(bind=engine)
