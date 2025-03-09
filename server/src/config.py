5abecb2152e7f5e8a61f95cb0325e0a44fb1ab78
Roma4286 <lionkrou8@gmail.com>
2025-03-09 22:15:11 +0300

    init: Initial project structure

---
+++ server/src/config.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+from pydantic import BaseModel
+from pydantic.v1 import BaseSettings
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+BASE_DIR = Path(__file__).parent.parent.parent
+
+
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 120
+
+class Settings(BaseSettings):
+    db_url: str = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+    auth_jwt: AuthJWT = AuthJWT()
+
+settings = Settings()
