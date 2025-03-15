aad34035b72b35d60b37b0c4c03412193c2507bb
Roma4286 <lionkrou8@gmail.com>
2025-03-15 22:28:41 +0300

    feat(db): add news table, update users table

--- server/src/models.py
+++ server/src/models.py
@@ -1,19 +1,49 @@
-from sqlalchemy import String
+from datetime import datetime
+
+from pydantic import BaseModel
+from sqlalchemy import String, Integer, Text, Boolean, JSON
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database import Base, engine
+
+class History(BaseModel):
+    date: datetime
+    misinformation_level_delta: int
+    pollution_delta: int
+    trust_science_delta: int
+    answers: list
 
 class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(30))
-    password: Mapped[str] = mapped_column(String(255))
+    password: Mapped[str] = mapped_column(String(30))
+    misinformation_level: Mapped[int] = mapped_column(Integer, default=0)
+    pollution: Mapped[int] = mapped_column(Integer, default=0)
+    trust_science: Mapped[int] = mapped_column(Integer, default=0)
+    history: Mapped[list] = mapped_column(JSON, default=list)
 
     @classmethod
-    def get_object(cls, session, id=None, username=None, password=None, role=None):
+    def get_object(cls, session, id=None, username=None, password=None):
         query = super().get_object(session=session, id=id, username=username, password=password)
         return query
 
+class News(Base):
+    __tablename__ = "news"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    text: Mapped[str] = mapped_column(Text())
+    explanation: Mapped[str] = mapped_column(Text())
+    is_true: Mapped[bool] = mapped_column(Boolean, default=True)
+    misinformation_level_delta: Mapped[int] = mapped_column(Integer)
+    pollution_delta: Mapped[int] = mapped_column(Integer)
+    trust_science_delta: Mapped[int] = mapped_column(Integer)
+
+    @classmethod
+    def get_object(cls, session, id=None, name=None, text=None, explanation=None, is_true=None, disinformation_level_delta=None, pollution_delta=None, trust_science_delta=None):
+        query = super().get_object(session=session, id=id, name=name, text=None, explanation=None, is_true=is_true, disinformation_level_delta=disinformation_level_delta, pollution_delta=pollution_delta, trust_science_delta=trust_science_delta)
+        return query
 
 # Base.metadata.create_all(bind=engine)
