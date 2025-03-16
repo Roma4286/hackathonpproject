c6d1ec394c90e5cac6abb431a9e6164e093e0264
Roma4286 <lionkrou8@gmail.com>
2025-03-16 19:42:40 +0300

    feat: add docker

--- server/src/config.py
+++ server/src/config.py
@@ -16,7 +16,7 @@ DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 
-BASE_DIR = Path(__file__).parent.parent.parent
+BASE_DIR = Path(__file__).parent.parent
 
 
 class RedisDb(BaseModel):
